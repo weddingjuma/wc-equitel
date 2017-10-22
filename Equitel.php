fd124bf432947aabe28b4f0ce78904d76082671a
Mauko Maunde <maukoese@gmail.com>
2017-10-22 11:27:48 +0300

    Added Methods code

--- Equitel.php
+++ Equitel.php
@@ -26,22 +26,74 @@ class Equitel
 		$url = "https://api.equitybankgroup.com/identity/v1-sandbox/token";
 	}
 
-	public function purchaseAirtime( $value = '' )
+	public function purchaseAirtime( $mobileNumber, $amount, $reference, $telco = "equitel" )
 	{
 		$url = "https://api.equitybankgroup.com/transaction/v1-sandbox/airtime";
+		$data = array (
+		    "customer" => array (
+		        "mobileNumber" => $mobileNumber
+		    ),
+		    "airtime" => array (
+		        "amount" => $amount,
+		        "reference" => $reference,
+		        "telco" => $telco
+		    )
+		);
+
+		$result = $ajax -> post( $url, json_encode( $data ) );
+
+		return json_decode( $result );
 	}
 
 	//Allows a Remittance Agent to transfer money to a Recipient Account in real-time.
-	public function onlineRemittance(){
+	public function remit(){
 		$url = "https://api.equitybankgroup.com/transaction/v1-sandbox/remittance";
+
+		$data = array (
+		    "transactionReference" => "",
+		    "source" => array (
+		        "senderName" => ""
+		    ),
+		    "destination" => array (
+		        "accountNumber" => "",
+		        "bicCode" => "",
+		        "mobileNumber" => "",
+		        "walletName" => "",
+		        "bankCode" => "",
+		        "branchCode" => ""
+		    ),
+		    "transfer" => array (
+		        "countryCode" => "",
+		        "currencyCode" => "",
+		        "amount" => "",
+		        "paymentType" => "",
+		        "paymentReferences" => [ "" ],
+		        "remarks" => ""
+		    )
+		);
 	}
 
 	//A merchant can view the latest status of a transaction being processed
-	public function paymentStatus( $transactionId ){
+	public function status( $transactionId ){
 		$url = "https://api.equitybankgroup.com/transaction/v1-sandbox/payments/".$transactionId;
+
+		$data = $ajax -> get( $url );
+
+		return $data;
 	}
 
-	public function createPayment(){
+	public function pay(){
 		$url = "https://api.equitybankgroup.com/transaction/v1-sandbox/payments";
+		$data = array (
+		    "customer" => array (
+		        "mobileNumber": ""
+		    ),
+		    "transaction" => array (
+		        "amount" => "",
+		        "description" => "",
+		        "type" => "",
+		        "auditNumber" => ""
+		    )
+		)
 	}
 }
--- README.md
+++ README.md
@@ -1,9 +1,9 @@
-# MPESA For WooCommerce
-WordPress Plugin that extends WooCommerce with MPESA Functionality for payments, transactions and balance checking.
+# Equitel For WooCommerce
+WordPress Plugin that extends WooCommerce with Equitel Functionality for payments, transactions and balance checking.
 ![alt text](https://user-images.githubusercontent.com/14233942/31839718-fefe17ca-b5ea-11e7-9afa-95fa395a8e43.png)
 
 # Installation
-* First, you need to download the latest release of the plugin from [here](https://github.com/maukoese/wc-mpesa/releases). Next, you need to go to WordPress admin area and visit Plugins » Add New page.
+* First, you need to download the latest release of the plugin from [here](https://github.com/maukoese/wc-equitel/releases). Next, you need to go to WordPress admin area and visit Plugins » Add New page.
 * After that, click on the Upload Plugin button on top of the page.
 * This will bring you to the plugin upload page. Here you need to click on the choose file button and select the plugin file you downloaded earlier to your computer.
 * After you have selected the file, you need to click on the install now button.
